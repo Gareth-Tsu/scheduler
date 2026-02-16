d2c362b35e6e7b0081e364b5d1d6b50738a3a09c
Gareth-tsu <Gareth.tsbira@gmail.com>
2026-02-16 17:28:02 -0600

    Added tests for Day class

--- Scheduler/logic.py
+++ Scheduler/logic.py
@@ -1,14 +1,18 @@
-
 from datetime import datetime
 
 time_pattern = r'^\d{1,2}:\d{1,2}$'
 days_of_week = {'monday': 0, 'tuesday': 1, 'wednesday': 2, 'thursday': 3, 'friday': 4, 'saturday': 5, 'sunday': 6}
-id_tags= []
+id_tags = []
+
+
 class TimeConflict(Exception):
-    print("There was a time conflict")
+    pass
+
 
 class Day:
     def __init__(self, day: str):
+        if day.lower() not in days_of_week:
+            raise IndexError
         self.day_name = day
         self.number = days_of_week[day.lower()]
         self.events = []
@@ -21,20 +25,25 @@ class Day:
         return False
 
     def add_event(self, event):
-        if self.has_conflict(event):
+        if not self.has_conflict(event):
             self.events.append(event)
             self.events.sort(key=lambda e: e.start_time)
-
         else:
             raise TimeConflict
 
     def remove_event(self, event):
-        self.events.remove(event)
-        self.events.sort(key=lambda e: e.start_time)
+        if len(self.events) > 0:  # no events to remove
+            if event in self.events:
+                self.events.remove(event)
+                self.events.sort(key=lambda e: e.start_time)
+            else:
+                raise KeyError
+        else:
+            raise IndexError
+
 
     def list_events(self):
         return self.events
-
 
     def __str__(self):
         return self.day_name
@@ -65,7 +74,8 @@ class Event:
 
     def __repr__(self):
         return (
-            f"Event: {self.title}, for {self.duration.total_seconds() // 3600} hours, at {self.start_time}, till {self.end_time}, "
+            f"Event: {self.title}, for {self.duration.total_seconds() // 3600} hours,"
+            f" at {self.start_time}, till {self.end_time}, "
             f"at {self.location}, on {self.day}")
 
 
@@ -73,9 +83,7 @@ class User:
     # User needs to be able to have default events as well as have a week and a timeline and a schedule
     def __init__(self, name):
         self.name = name
-        self.timeslots = {x: [] for x in range((7*24))}
         self.week = [Day(x) for x in days_of_week]
-
 
     def add_default_event(self, event):
         pass
--- Scheduler/test_logic.py
+++ Scheduler/test_logic.py
@@ -17,6 +17,7 @@ def test_event_init():
     assert e.location == "missouri state"
     assert e.duration == e.end_time - e.start_time
 
+
 def test_event_parse_time():
     # Test to make sure start and end times entered with an invalid format raises an error
     with pytest.raises(ValueError):
@@ -44,6 +45,7 @@ def test_event_parse_time():
             "missouri state"
         )
 
+
 def test_event_valid_duration_raises():
     # Test to make sure new events raise errors when given start and end times that don't work
     with pytest.raises(TimeConflict):
@@ -65,3 +67,74 @@ def test_event_valid_duration_raises():
             "missouri state"
         )
 
+
+def test_day_init():
+    d = Day("tuesday")
+    assert d.day_name == "tuesday"
+    with pytest.raises(IndexError):
+        Day("Hello world!")
+
+
+def test_day_has_conflict():
+    d = Day("monday")
+    # e is the control event
+    e = Event(
+            "Monday",
+            "11:00",
+            "12:00",
+            "class",
+            "missouri state"
+        )
+    # e2 is an event that starts while the control event is going
+    e2 = Event(
+        "monday",
+        "11:30",
+        "12:30",
+        "gym",
+        "gym"
+    )
+    # e3 is the event that ends after the control event starts
+    e3 = Event(
+        "monday",
+        "10:00",
+        "11:45",
+        "class",
+        "school"
+    )
+    # e4 is the event that starts and ends during the duration of the control event
+    e4 = Event(
+        "monday",
+        "11:15",
+        "11:45",
+        "title",
+        "place"
+    )
+    d.add_event(e)
+    with pytest.raises(TimeConflict):
+        d.add_event(e2)
+    with pytest.raises(TimeConflict):
+        d.add_event(e3)
+    with pytest.raises(TimeConflict):
+        d.add_event(e4)
+
+def test_day_remove_event_raises():
+    e = Event(
+        "monday",
+        "11:15",
+        "11:45",
+        "title",
+        "place"
+    )
+    e2 = Event(
+        "monday",
+        "11:15",
+        "11:45",
+        "title",
+        "place"
+    )
+    d = Day("monday")
+    with pytest.raises(IndexError):
+        d.remove_event(e)
+    d.add_event(e)
+    with pytest.raises(KeyError):
+        d.remove_event(e2)
